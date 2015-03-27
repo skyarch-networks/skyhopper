ebd26732b4a26f1524eb620437fb4159a788fb64
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-03-27 15:25:21 +0900

    Spec

--- spec/jobs/serverspec_job_spec.rb
+++ spec/jobs/serverspec_job_spec.rb
@@ -1,5 +1,29 @@
 require_relative '../spec_helper'
 
 RSpec.describe ServerspecJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#perform' do
+    let(:infra){create(:infrastructure)}
+    let(:physical_id){SecureRandom.base64(10)}
+    let(:serverspecs){create_list(:serverspec, 3)}
+    let(:resource){create(:resource, physical_id: physical_id, infrastructure: infra, serverspecs: serverspecs)}
+
+    before do
+      resource
+    end
+
+    it 'should call Node#run_serverspec' do
+      expect_any_instance_of(Node).to receive(:run_serverspec).with(infra.id, serverspecs.map(&:id), false)
+      ServerspecJob.perform_now(physical_id, infra.id)
+    end
+
+    context 'when error' do
+      before do
+        expect_any_instance_of(Node).to receive(:run_serverspec).with(infra.id, serverspecs.map(&:id), false).and_raise
+      end
+
+      it 'should call Node#run_serverspec' do
+        ServerspecJob.perform_now(physical_id, infra.id)
+      end
+    end
+  end
 end
