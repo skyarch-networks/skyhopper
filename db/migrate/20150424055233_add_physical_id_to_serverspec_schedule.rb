cebd61ad3259f24485589a5cb71da5ae1b8a46ce
candy <lovingrootbeer@gmail.com>
2015-04-30 16:32:17 +0900

    Change a NOT NULL column to allow NULL

--- db/migrate/20150424055233_add_physical_id_to_serverspec_schedule.rb
+++ db/migrate/20150424055233_add_physical_id_to_serverspec_schedule.rb
@@ -1,5 +1,5 @@
 class AddPhysicalIdToServerspecSchedule < ActiveRecord::Migration
   def change
-    add_column :serverspec_schedules, :physical_id, :string, null: false
+    add_column :serverspec_schedules, :physical_id, :string
   end
 end
