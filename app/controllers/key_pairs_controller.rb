040610dbe888e424d92a7cae633dad329f7709d2
candy <lovingrootbeer@gmail.com>
2015-05-20 14:11:35 +0900

    Fix KeyPairsController

--- app/controllers/key_pairs_controller.rb
+++ app/controllers/key_pairs_controller.rb
@@ -1,14 +1,23 @@
 class KeyPairsController < ApplicationController
+  include Concerns::BeforeAuth
   # --------------- Auth
   before_action :authenticate_user!
 
+  before_action do
+    project_id = params[:id]
+    @allow_change = current_user.admin? and allowed_project(project_id)
+  end
+
   def index
     @project_id = params.require(:id)
-
     @project = Project.find(@project_id)
   end
 
   def delete_key_pair
+    unless current_user.admin?
+      render text: '', status: 403 and return
+    end
+
     @project_id = params.require(:id)
     @region     = params.require(:region)
     @key_name   = params.require(:name)
@@ -20,7 +29,6 @@ class KeyPairsController < ApplicationController
       secret_access_key: project.secret_access_key,
       region:            @region
     )
-
     ec2.delete_key_pair(key_name: @key_name)
 
     render text: '', status: 200 and return
