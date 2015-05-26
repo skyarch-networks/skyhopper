9eb94c69fbf1c48337927e32436421a342c53db0
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-05-26 16:46:45 +0900

    Fix CloudFormation View

--- app/policies/cf_template_policy.rb
+++ app/policies/cf_template_policy.rb
@@ -1,8 +1,8 @@
 class CfTemplatePolicy < ApplicationPolicy
   %i[edit? update? destroy?].each do |action|
     define_method(action) do
-      if infra = record.infrastructure
-        user.allow?(infra)
+      if record.infrastructure_id
+        user.allow?(record.infrastructure)
       else
         user.master and user.admin
       end
@@ -11,8 +11,8 @@ class CfTemplatePolicy < ApplicationPolicy
 
   %i[new_for_creating_stack? insert_cf_params? create_and_send? history? show? index?].each do |action|
     define_method(action) do
-      if infra = record.infrastructure
-        user.allow?(infra)
+      if record.infrastructure_id
+        user.allow?(record.infrastructure)
       else
         true
       end
--- app/policies/template_builder_policy.rb
+++ app/policies/template_builder_policy.rb
@@ -1,7 +1,3 @@
 class TemplateBuilderPolicy < ApplicationPolicy
-  %i[new? resource_properties? create?].each do |action|
-    define_method(action) do
-      user.master and user.admin
-    end
-  end
+  master_admin :new?, :resource_properties?, :create?
 end
