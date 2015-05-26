91b56c48137b772f8284c28da01d8c40e3493667
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-05-26 11:18:54 +0900

    spec performance :rocket:

--- spec/policies/template_builder_policy_spec.rb
+++ spec/policies/template_builder_policy_spec.rb
@@ -7,10 +7,10 @@ describe TemplateBuilderPolicy do
 
   %i[new? resource_properties? create?].each do |action|
     permissions action do
-      let(:master_admin){create(:user, master: true,  admin: true)}
-      let(:master){      create(:user, master: true,  admin: false)}
-      let(:admin){       create(:user, master: false, admin: true)}
-      let(:normal){      create(:user, master: false, admin: false)}
+      let(:master_admin){build(:user, master: true,  admin: true)}
+      let(:master){      build(:user, master: true,  admin: false)}
+      let(:admin){       build(:user, master: false, admin: true)}
+      let(:normal){      build(:user, master: false, admin: false)}
 
       it 'should grant only master and admin' do
         is_expected.to     permit(master_admin, builder)
