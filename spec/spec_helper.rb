96a8ae39f4fd5c9c65330dd005107764fcb076ee
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-05-27 17:28:15 +0900

    Remove unused include

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,8 +48,6 @@ RSpec.configure do |config|
   #     --seed 1234
   #config.order = "random"
 
-  config.include Capybara::DSL, :type => :feature
-
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
   config.include Devise::TestHelpers, type: :view
