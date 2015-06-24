44a287b6b10fb73384498c110ccb6a3454285ce9
candy <lovingrootbeer@gmail.com>
2015-06-24 13:28:40 +0900

    Refactor ServerspecSchedule model

--- app/models/serverspec_schedule.rb
+++ app/models/serverspec_schedule.rb
@@ -6,36 +6,14 @@
 # http://opensource.org/licenses/mit-license.php
 #
 
-require 'sidekiq/api'
-
 class ServerspecSchedule < ActiveRecord::Base
+  include Concerns::Schedule
+
   belongs_to :resource, foreign_key: 'physical_id', primary_key: 'physical_id'
 
-  enum frequency:   %i[daily weekly intervals]
-  enum day_of_week: %i[sunday monday tuesday wednesday thursday friday saturday]
+  @@job_class_name = PeriodicServerspecJob.to_s.freeze
 
-  validates :frequency, inclusion: { in: frequencies }, if: :enabled
-  validates :time, numericality: { greater_than: 0 }, if: -> { enabled && frequency == 'intervals' }
-
-  before_update :delete_enqueued_jobs
-  after_destroy :delete_enqueued_jobs
-
-  def next_run
-    case self.frequency
-    when 'weekly'
-      ntime = Time.current.beginning_of_week(:sunday) + self[:day_of_week].days + self.time.hours
-      ntime = ntime + 1.week if ntime.past?
-    when 'daily'
-      ntime = Time.current.beginning_of_day + self.time.hours
-      ntime = ntime.tomorrow if ntime.past?
-    when 'intervals'
-      ntime = Time.current.beginning_of_hour + self.time.hours
-    end
-    ntime
-  end
-
-  def delete_enqueued_jobs
-    jobs = Sidekiq::ScheduledSet.new.select { |job| job.args[0]['arguments'][0] == self.physical_id }
-    jobs.each(&:delete)
+  def job_class_name
+    @@job_class_name
   end
 end
