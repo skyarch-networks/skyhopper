85cfe68c4723d8182d21d2a79bce0558038c2f3c
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-07-15 11:33:09 +0900

    [FIX] Enable to work tooltip

--- app/assets/javascripts/helper.js
+++ app/assets/javascripts/helper.js
@@ -116,7 +116,7 @@ var loadGif, glyphicon, bootstrap_confirm, bootstrap_alert, bootstrap_prompt, mo
   };
 
 
-  // <bootstrap-tooltip v-with="title: 'tooltip title'">
+  // <bootstrap-tooltip title="'tooltip title'">
   //   <div>Your content</div>
   // </bootstrap-tooltip>
   Vue.component('bootstrap-tooltip', {
