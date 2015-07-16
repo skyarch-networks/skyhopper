76242e15c0cb002fecffb6b57343b33477d6d6ca
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-07-16 18:27:59 +0900

    s3 model

---
+++ frontend/src/models/s3_bucket.ts
@@ -0,0 +1,24 @@
+/// <reference path="../../typings/tsd.d.ts" />
+/// <reference path="../../ajax_set.d.ts" />
+
+class S3Bucket {
+  constructor(private infra: any, private pyhsical_id: string) {
+
+  }
+
+  static ajax = new AjaxSet.Resources('infrastructures');
+
+  show(): JQueryPromise<any> {
+    var dfd = $.Deferred();
+
+    (<any>S3Bucket.ajax).show_s3({
+      bucket_name: this.pyhsical_id,
+      id: this.infra.id,
+    }).done((data: any) => dfd.resolve(data))
+      .fail((xhr: XMLHttpRequest) => dfd.reject(xhr.responseText));
+
+    return dfd.promise();
+  }
+}
+
+S3Bucket.ajax.add_member("show_s3", "GET");
