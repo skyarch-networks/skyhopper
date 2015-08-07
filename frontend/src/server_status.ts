7980246e5b68c8da855f94c14dd78543678b3083
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-08-07 11:37:20 +0900

    ServerState.App extends Vue class.

--- frontend/src/server_status.ts
+++ frontend/src/server_status.ts
@@ -30,25 +30,19 @@ module ServerStatus {
      * @method msgs
      * @return {Object} I18n した結果を複数格納した辞書
      */
-    msgs(): any {
-      return t('js.server_status.' + this.kind);
-    }
+    msgs(): any { return t('js.server_status.' + this.kind); }
 
     /**
      * Start server.
      * @method start
      */
-    start(): void {
-      (<any>Server.ajax).start(this.params);
-    }
+    start(): void { (<any>Server.ajax).start(this.params); }
 
     /**
      * Stop server.
      * @method stop
      */
-     stop(): void {
-       (<any>Server.ajax).stop(this.params);
-     }
+     stop(): void { (<any>Server.ajax).stop(this.params); }
 
     /**
      * Get server status.
@@ -83,92 +77,114 @@ module ServerStatus {
   Server.ajax.add_member('status', 'POST', 'kind');
 
 
+  class App extends Vue {
+    private state: string;
+    constructor(private model: Server) {
+      super();
+      this._init({
+        data: {state: this.state},
+        template: TEMPLATE_ID,
+        methods: {
+          start:  this.start,
+          stop:   this.stop,
+          status: this.status,
+          toggle: this.toggle,
+        },
+        computed: {
+          running:       this.running,
+          stopped:       this.stopped,
+          is_inprogress: this.is_inprogress,
+          status_text:   this.status_text,
+          btn_class:     this.btn_class,
+          tooltip:       this.tooltip,
+        },
+        created: this._created,
+      });
+    }
 
-  // View Model Factory.
-  const new_toggle_button = function (model: Server) {
-    return new Vue({
-      template: TEMPLATE_ID,
-      data: {state: ""},
+    /// ===  Instance Methods
 
-      methods: {
-        // confirm and start server.
-        start: function (): void {
-          bootstrap_confirm(model.msgs().title, model.msgs().confirm_start).done(() => {
-            model.start();
-            this.status(true);
+    // confirm and start server.
+    start(): void {
+      bootstrap_confirm(this.model.msgs().title, this.model.msgs().confirm_start).done(() => {
+        this.model.start();
+        this.status(true);
+      });
+    }
+
+    // confirm and stop server.
+    stop(): void {
+      bootstrap_confirm(this.model.msgs().title, this.model.msgs().confirm_stop).done(() => {
+        this.model.stop();
+        this.status(true);
+      });
+    }
+
+    // Update ViewModel state, and watch sate change.
+    status(background?: boolean): void {
+      this.model.status(background).done((state: string) => {
+        this.state = state;
+        if (background || this.is_inprogress) {
+          this.model.watch((w_state: string) => {
+            this.state = w_state;
           });
-        },
+        }
+      });
+    }
 
-        // confirm and stop server.
-        stop: function (): void {
-          bootstrap_confirm(model.msgs().title, model.msgs().confirm_stop).done(() => {
-            model.stop();
-            this.status(true);
-          });
-        },
+    // toggle server state.
+    toggle(): void {
+      // XXX
+      (<any>this).$event.preventDefault();
+      if (this.running) {
+        this.stop();
+      } else if (this.stopped) {
+        this.start();
+      }
+    }
 
-        // Update ViewModel state, and watch sate change.
-        status: function (background: boolean): void {
-          model.status(background).done((state: string) => {
-            this.state = state;
-            if (background || this.is_inprogress) {
-              model.watch((w_state: string) => {
-                this.state = w_state;
-              });
-            }
-          });
-        },
 
-        // toggle server state.
-        toggle: function (): void {
-          this.$event.preventDefault();
-          if (this.running) {
-            this.stop();
-          } else if (this.stopped) {
-            this.start();
-          }
-        },
-      },
-      computed: {
-        running:       function () { return this.state === 'running'; },
-        stopped:       function () { return this.state === 'stopped'; },
-        is_inprogress: function () { return Server.is_inprogress(this.state); },
+    /// ===  Computed methods.
+    running():       boolean { return this.state === 'running'; }
+    stopped():       boolean { return this.state === 'stopped'; }
+    is_inprogress(): boolean { return Server.is_inprogress(this.state); }
 
-        status_text: function (): string {
-          let res = _.capitalize(model.kind) + ' Server ';
-          if (this.running) {
-            res += 'is ' + this.state + '.';
-          } else if (this.stopped) {
-            res += 'was ' + this.state + '.';
-          } else if (this.is_inprogress) {
-            res += this.state + '...';
-          }
-          return res;
-        },
+    status_text(): string {
+      let res = _.capitalize(this.model.kind) + ' Server ';
+      if (this.running) {
+        res += 'is ' + this.state + '.';
+      } else if (this.stopped) {
+        res += 'was ' + this.state + '.';
+      } else if (this.is_inprogress) {
+        res += this.state + '...';
+      }
+      return res;
+    }
 
-        btn_class: function (): string {
-          if (this.running) {
-            return 'btn-success';
-          } else if (this.stopped) {
-            return 'btn-default';
-          } else if (this.is_inprogress) {
-            return 'btn-warning';
-          }
-        },
+    btn_class(): string {
+      if (this.running) {
+        return 'btn-success';
+      } else if (this.stopped) {
+        return 'btn-default';
+      } else if (this.is_inprogress) {
+        return 'btn-warning';
+      }
+    }
 
-        tooltip: function (): string {
-          if (this.is_inprogress) { return ""; }
-          return model.msgs().tooltip;
-        },
-      },
-      created: function () {
-        this.status();
-      },
-    });
-  };
+    tooltip(): string {
+      if (this.is_inprogress) { return ""; }
+      return this.model.msgs().tooltip;
+    }
+
+    // Lifecycle
+    private _created(): void {
+      console.log(this);
+      this.status();
+    }
+  }
 
   export const build = function (kind: string): void {
-    const vm = new_toggle_button(new Server(kind));
+    const vm = new App(new Server(kind));
     const e = document.createElement('div');
     vm.$mount(e);
     vm.$after(TEMPLATE_ID);
