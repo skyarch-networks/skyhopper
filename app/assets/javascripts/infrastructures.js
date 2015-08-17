c149fb76afeea10bacb153dd17878e0e365e8c6f
Joeper214 <joeper.serrano@gmail.com>
2015-08-17 11:11:30 +0900

    Kuwabara: I think data.templates does not become null/undefined/empty. Because templates is hardcorded.

--- app/assets/javascripts/infrastructures.js
+++ app/assets/javascripts/infrastructures.js
@@ -408,7 +408,7 @@
         self.commons         = data.monitor_selected_common;
         self.uncommons       = data.monitor_selected_uncommon;
         self.resources       = data.resources;
-        self.templates       = data.templates || [];
+        self.templates       = data.templates;
 
         if (!this.before_register) {
           self.show_problems();
