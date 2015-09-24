8fd458045ed33f6f6eda15ebe47b7b24e1f20dad
candy <lovingrootbeer@gmail.com>
2015-09-24 13:23:39 +0900

    Fix RSpec for Node model

--- spec/models/node_spec.rb
+++ spec/models/node_spec.rb
@@ -33,9 +33,10 @@ describe Node, :type => :model do
     end
 
     let(:infra){build(:infrastructure)}
+    let(:whyrun){false}
 
     it "returns true if status is success" do
-      expect(subject.cook(infra)).to be_truthy
+      expect(subject.cook(infra, whyrun)).to be_truthy
     end
   end
 
