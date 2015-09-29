30f4626285077632a2430e85cfe9bf209567c1ee
Joeper214 <joeper.serrano@gmail.com>
2015-09-29 07:27:16 +0000

    forgot to add ace.js file

---
+++ app/assets/javascripts/modules/ace.js
@@ -0,0 +1,34 @@
+exports.install = function(Vue, options, mode){
+  Vue.directive("ace", {
+      twoWay: true,
+      bind: function () {
+          this.editor = ace.edit(this.el);
+          this.editor.setTheme("ace/theme/github");
+          this.editor.getSession().setMode("ace/mode/"+mode);
+          this.editor.getSession().setUseWrapMode(true);
+          this.editor.$blockScrolling = Infinity;
+          this.editor.setOptions({
+            maxLines: Infinity,
+            minLines: 15,
+          });
+          if (options){
+            this.editor.setOptions({readOnly: options, highlightActiveLine: false, highlightGutterLine: false});
+            this.editor.renderer.$cursorLayer.element.style.opacity=0;
+          }
+
+          this.silent = false;
+          this.handler = function () {
+              if (!this.silent) {
+                  this.set(this.editor.getSession().getValue(), true);
+              }
+          }.bind(this);
+          this.editor.on("change", this.handler);
+      },
+      update: function (value, oldValue) {
+          this.silent = true;
+          this.editor.getSession().setValue(value);
+          this.silent = false;
+      }
+  });
+
+};
