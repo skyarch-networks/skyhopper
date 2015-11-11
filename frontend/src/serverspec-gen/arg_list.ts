077f3e11726f56b5d0d59acf0e53cd7b558f1e02
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-11-11 11:17:08 +0900

    WIP

--- frontend/src/serverspec-gen/arg_list.ts
+++ frontend/src/serverspec-gen/arg_list.ts
@@ -9,12 +9,22 @@ export default Vue.extend({
       type: Array,
       required: true,
     },
+    results: {
+      // [{name: String, arg: String}, ...]
+      type: Array,
+      required: true,
+      twoWay: true,
+    },
   },
-  methods: {
-  },
-  computed: {
-  },
+
+  methods: { },
+  computed: { },
+
   ready: function() {
+    (<string[]>this.args).forEach((n, idx) => {
+      const v = {name: n, arg: ""};
+      this.results[idx] = v;
+    });
     console.log(this);
   },
 });
