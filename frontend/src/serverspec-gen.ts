7d01bd6f71fae7063507d12deb4ed61c0792451a
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-12-10 11:34:09 +0900

    Save generated serverspec.

--- frontend/src/serverspec-gen.ts
+++ frontend/src/serverspec-gen.ts
@@ -1,18 +1,24 @@
-import * as Gen from "serverspec-generator";
-
+import * as Gen   from "serverspec-generator";
+import {Prompt, Alert, AlertForAjaxStdError}   from './modal';
+import Serverspec from './models/serverspec';
+import * as qs from 'query-string';
 
 const app = new Gen.App([], Gen.Info.value);
 console.log(app);
 
-// TODO
-$(document).on('click', '#save-btn', () => {
-  const file = new File([app.rubyCode], 'generated_spec.rb');
-  const url = window.URL.createObjectURL(file);
-  const a = document.createElement('a');
-  a.href = url;
-  a.setAttribute('download', file.name);
-  document.body.appendChild(a);
-  a.click();
+$(document).on('click', '.save-serverspec-btn', () => {
+  Prompt('Save Serverspec', "filename").then((fname) => {
+    const s = new Serverspec();
+    const infra_id_str: string = qs.parse(location.search).infrastructure_id;
+    const infra_id: number     = infra_id_str ? parseInt(infra_id_str) : null;
+    const code = app.rubyCode;
+    return s.create(fname, code, infra_id);
+  }).then(
+    data => Alert(t('serverspecs.serverspec'), data),
+    AlertForAjaxStdError()
+  ).then(() => {
+    location.href = `/serverspecs${location.search}`;
+  });
 });
 
 document.querySelector("#vue-serverspec-gen").appendChild(app.$el);
