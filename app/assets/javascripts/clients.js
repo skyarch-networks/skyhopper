e06c843fbb0909b82aad750d557286089d94fbab
Joeper214 <Joeper.Serrano@gmail.com>
2015-12-11 15:02:06 +0900

    added no results found message on client, project and infra index

--- app/assets/javascripts/clients.js
+++ app/assets/javascripts/clients.js
@@ -38,6 +38,7 @@
         lang: queryString.lang,
         pages: 10,
         pageNumber: 0,
+        filteredLength: null,
         };
       },
     methods: {
@@ -86,6 +87,7 @@
              self.$emit('data-loaded');
              var empty = t('projects.msg.empty-list');
              if(self.data.length === 0){ $('#empty').show().html(empty);}
+             self.filteredLength = data.length;
            }
          });
          $("#loading").hide();
@@ -98,7 +100,13 @@
         return list.slice(index, index + this.pages);
       },
       roundup: function (val) { return (Math.ceil(val));},
-    }
+      count: function (arr) {
+        // record length
+        this.$set('filteredLength', arr.length);
+        // return it intact
+        return arr;
+      }
+    },
  });
 
 
