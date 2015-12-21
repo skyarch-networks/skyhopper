9298f95733f1762b315e52e180d929bf2f5b9752
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-12-21 14:45:34 +0900

    precompile only needed assets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,10 @@ SkyHopper::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( * )
+
+  # for controllers js
+  config.assets.precompile += Dir.glob(Rails.root.join('app','controllers', "*.rb")).map{|x| File.basename(x)[/^(.+)_controller\.rb/, 1]+".js"}
+  config.assets.precompile += ['edit_runlist.js']
   config.assets.initialize_on_precompile = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
