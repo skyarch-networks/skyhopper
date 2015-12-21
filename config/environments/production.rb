dae3865dd9a257a503d5962315b2e08ef27eb77e
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-12-21 14:52:26 +0900

    forgotten libraries

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ SkyHopper::Application.configure do
 
   # for controllers js
   config.assets.precompile += Dir.glob(Rails.root.join('app','controllers', "*.rb")).map{|x| File.basename(x)[/^(.+)_controller\.rb/, 1]+".js"}
-  config.assets.precompile += ['edit_runlist.js']
+  config.assets.precompile += %w[edit_runlist.js libraries.js]
   config.assets.initialize_on_precompile = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
