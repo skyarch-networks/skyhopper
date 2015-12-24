f25934377b09926ede2fe90d341ad6f6dfd21575
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-12-24 16:17:26 +0900

    Remove flag

--- frontend/src/project_parameters.ts
+++ frontend/src/project_parameters.ts
@@ -3,18 +3,58 @@ interface Parameter {
   key: string;
   value: string;
   project_id: number;
+  remove: boolean;
+}
+
+interface Project {
+  id: number;
 }
 
 declare const PROJECT_PARAMETERS: Parameter[];
+declare const PROJECT: Project;
 
-const App = new Vue({
-  el: '#project-parameter-index',
-  data: {
-    params: PROJECT_PARAMETERS,
+PROJECT_PARAMETERS.forEach((p) => {
+  p.remove = false;
+});
+
+Vue.component('param-tr', {
+  template: '#param-tr-template',
+  data: () => {return {}; },
+  props: {
+    param: {
+      twoWay: true,
+      required: true,
+      type: Object,
+    },
   },
-  methods: {
 
+  methods: {
+    remove:   function () { this.param.remove = true; },
+    unremove: function () { this.param.remove = false; },
   },
 });
 
-console.log(App);
+const ProjectParamApp = new Vue({
+  el: '#project-parameter-index',
+  data: {
+    params: PROJECT_PARAMETERS,
+    project: PROJECT,
+  },
+  methods: {
+    add: function () {
+      this.params.push({
+        id: null,
+        key: 'KEY',
+        value: 'VALUE',
+        project_id: this.project.id,
+        remove: false,
+      });
+    },
+
+    save: function () {
+      // TODO
+    },
+  },
+});
+
+console.log(ProjectParamApp);
