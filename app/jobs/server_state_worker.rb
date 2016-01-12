2312433337075a6af36bb9a7e9bf4c2a0b8e0a85
candy <lovingrootbeer@gmail.com>
2016-01-12 13:14:35 +0900

    Refactor ServerStateWorker

--- app/jobs/server_state_worker.rb
+++ app/jobs/server_state_worker.rb
@@ -2,11 +2,8 @@
 class ServerStateWorker
   def perform
     %w[chef zabbix].each do |kind|
-      infra = Project.__send__("for_#{kind}_server").infrastructures.last
-      resources = infra.resources_or_create
-      physical_id = resources.first.physical_id
-      server = infra.instance(physical_id)
-      Rails.cache.write("serverstate-#{kind}", server.status)
+      server = ServerState.new(kind)
+      status = server.latest_status
     end
   end
 end
