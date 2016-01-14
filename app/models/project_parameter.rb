3e666fe572dd92249a382cdea7218c2826cf2aeb
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-01-14 14:53:12 +0900

    Execution project parameter

--- app/models/project_parameter.rb
+++ app/models/project_parameter.rb
@@ -10,4 +10,21 @@ class ProjectParameter < ActiveRecord::Base
   belongs_to :project
   validates :project_id, uniqueness: {scope: [:key]}
   validates :key, format: { with: /\A[a-zA-Z_][a-zA-Z0-9_]*\Z/ }
+
+  class << self
+    # @param [String] target
+    # @param [Project] project
+    # @param [Integer] project_id
+    # @return [String]
+    def exec(target, project: nil, project_id: nil)
+      raise ArgumentError if !(project_id || project)
+
+      params = self.where(project_id: project_id || project.id)
+      re = /(?<!\\)\$\{(?<key>#{params.map(&:key).join('|')})\}/
+      target.gsub(re) do |matched|
+        key = $~[:key]
+        params.find_by(key: key).value
+      end
+    end
+  end
 end
