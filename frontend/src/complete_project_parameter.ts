ec684c98ac13b9708511a94e09f6d12ed5dbbd4c
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-01-14 16:57:21 +0900

    Fast request

--- frontend/src/complete_project_parameter.ts
+++ frontend/src/complete_project_parameter.ts
@@ -1,15 +1,17 @@
 function strategyGen(project_id: number) {
+  const ajax = $.ajax({
+    url: '/project_parameters.json',
+    method: 'GET',
+    data: {
+      project_id: project_id,
+    }
+  });
+
   return {
     match: /\$\{((?:[a-zA-Z_][a-zA-Z0-9_]*)?)$/,
 
     search: function (term: string, callback: Function) {
-      $.ajax({
-        url: '/project_parameters.json',
-        method: 'GET',
-        data: {
-          project_id: project_id,
-        }
-      }).then((params: Array<any>) => {
+      ajax.then((params: Array<any>) => {
         const p = params
           .map(pa => pa.key)
           .filter(k => k.indexOf(term) === 0);
@@ -20,6 +22,7 @@ function strategyGen(project_id: number) {
     template: function (value: string) {
       return '${' + value + '}';
     },
+
     replace: function (word: string) {
       return "${" + word + '}';
     },
