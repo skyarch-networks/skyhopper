9bd441d167b535f6c515244ba090f1eebc76b2b5
Joeper214 <Joeper.Serrano@gmail.com>
2016-01-27 10:48:45 +0900

    set rubocop target ruby version to 2.2

--- app/controllers/infrastructures_controller.rb
+++ app/controllers/infrastructures_controller.rb
@@ -394,12 +394,10 @@ class InfrastructuresController < ApplicationController
     path =
       if session[:project_id].present?
         infrastructures_path(project_id: session[:project_id])
+    elsif current_user.master?
+      clients_path
     else
-      if current_user.master?
-        clients_path
-      else
-        projects_path
-      end
+      projects_path
     end
 
     redirect_to path, alert: msg
