10f7b15ed4e41fb44d690727276cac4dedbfc156
candy <lovingrootbeer@gmail.com>
2016-01-29 17:52:53 +0900

    rubocop

--- app/models/ec2_instance.rb
+++ app/models/ec2_instance.rb
@@ -141,7 +141,7 @@ class EC2Instance < SimpleDelegator
     policies = RetentionPolicy.where(resource_id: volume_ids)
     policies.map { |e|
       [ e.resource_id,
-        { max_amount: e.max_amount }
+        { max_amount: e.max_amount },
       ]
     }.to_h
   end
