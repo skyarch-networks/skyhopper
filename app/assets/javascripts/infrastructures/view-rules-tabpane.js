0db2c429d051b404c61cadf16eedc6eb08e10ec9
candy <lovingrootbeer@gmail.com>
2016-02-23 18:29:53 +0900

    fix indentation

--- app/assets/javascripts/infrastructures/view-rules-tabpane.js
+++ app/assets/javascripts/infrastructures/view-rules-tabpane.js
@@ -69,57 +69,60 @@ module.exports = Vue.extend({
       var defaultFont = Object.keys(map)[0];
 
       var docDefinition = {
-        footer: function(currentPage, pageCount) {return {
-          text: currentPage.toString() + ' of ' + pageCount};},
-          content: [
-            {
+        footer: function(currentPage, pageCount) {
+          return {
+            text: currentPage.toString() + ' of ' + pageCount
+          };
+        },
+        content: [
+          {
             text: t('security_groups.title'),
             style: 'header',
             alignment: 'center'
-            },
-            {
-              text: [t('infrastructures.stackname')+': ', { text: this.$parent.$data.current_infra.stack.name+'\n',  bold: true}],
-              alignment: 'left'
-            },
-            {
-              text: [t('infrastructures.physical_id')+': ', { text: this.physical_id.toString()+'\n',  bold: true}],
-              alignment: 'left'
-            },
-            {
-              text: [t('infrastructures.date')+': ', { text: moment().format('YYYY-MM-DD, HH:mm:ss')+'\n',  bold: true}],
-              alignment: 'left'
-            },
-            tableRender(data)
-          ],
-          styles: {
-            header: {
-              fontSize: 16,
-              bold: true,
-              margin: [0, 0, 0, 10]
-            },
-            subheader: {
-              fontSize: 14,
-              bold: true,
-              margin: [0, 10, 0, 5]
-            },
-            tableExample: {
-              margin: [5, 5, 0, 15]
-            },
-            tableHeader: {
-              bold: true,
-              fontSize: 11,
-              color: 'black'
-            },
           },
-          defaultStyle: {
-            // alignment: 'justify'
-            fontSize: 10,
-            alignment: 'center',
-            font: defaultFont
+          {
+            text: [t('infrastructures.stackname')+': ', { text: this.$parent.$data.current_infra.stack.name+'\n',  bold: true}],
+            alignment: 'left'
           },
-          pageSize: 'A4',
-          pageOrientation: 'landscape',
-          pageMargins: [15, 30, 20, 30 ]
+          {
+            text: [t('infrastructures.physical_id')+': ', { text: this.physical_id.toString()+'\n',  bold: true}],
+            alignment: 'left'
+          },
+          {
+            text: [t('infrastructures.date')+': ', { text: moment().format('YYYY-MM-DD, HH:mm:ss')+'\n',  bold: true}],
+            alignment: 'left'
+          },
+          tableRender(data)
+        ],
+        styles: {
+          header: {
+            fontSize: 16,
+            bold: true,
+            margin: [0, 0, 0, 10]
+          },
+          subheader: {
+            fontSize: 14,
+            bold: true,
+            margin: [0, 10, 0, 5]
+          },
+          tableExample: {
+            margin: [5, 5, 0, 15]
+          },
+          tableHeader: {
+            bold: true,
+            fontSize: 11,
+            color: 'black'
+          },
+        },
+        defaultStyle: {
+          // alignment: 'justify'
+          fontSize: 10,
+          alignment: 'center',
+          font: defaultFont
+        },
+        pageSize: 'A4',
+        pageOrientation: 'landscape',
+        pageMargins: [15, 30, 20, 30 ]
 
       };
 
