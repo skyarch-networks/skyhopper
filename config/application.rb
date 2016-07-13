26bf7a6e3dc76a793cf0aebe833ee43408cca8dc
Joeper214 <joeper.serrano@gmail.com>
2016-07-13 14:00:29 +0900

    Removed default locale so that during first application run it will not override the language header

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module SkyHopper
     # config.active_record.default_timezone = :local
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :ja
     config.i18n.available_locales = [:en, :ja]
     I18n.enforce_available_locales = false
 
