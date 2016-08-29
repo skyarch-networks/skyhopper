1f10461e5a2b62fc85ebf9565e00ab0b2f6a6b58
Joeper214 <joeper.serrano@gmail.com>
2016-08-29 16:10:04 +0900

    Retrieve migration files and schema script. for crono jobs model

--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,17 @@ ActiveRecord::Schema.define(version: 20160802055903) do
     t.datetime "updated_at"
   end
 
+  create_table "crono_jobs", force: :cascade do |t|
+    t.string   "job_id",            limit: 255,   null: false
+    t.text     "log",               limit: 65535
+    t.datetime "last_performed_at"
+    t.boolean  "healthy"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  add_index "crono_jobs", ["job_id"], name: "index_crono_jobs_on_job_id", unique: true, using: :btree
+
   create_table "dishes", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.text     "runlist",    limit: 65535
