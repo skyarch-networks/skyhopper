b4a386be04c2208d39fbacb5a82ada70ec87d71a
Joeper214 <joeper.serrano@gmail.com>
2016-10-24 17:32:52 +0900

    Use an efficient query during project index load. Zabbix Server Helper testing

--- app/helpers/zabbix_servers_helper.rb
+++ app/helpers/zabbix_servers_helper.rb
@@ -1,11 +1,11 @@
 module ZabbixServersHelper
-  def edit_zabbix_server_path_url(zabbix_server)
-    return nil unless Pundit.policy(current_user, zabbix_server).edit?
+  def edit_zabbix_server_path_url(zabbix_server, user: current_user)
+    return nil unless Pundit.policy(user, zabbix_server).edit?
     return edit_zabbix_server_path(zabbix_server)
   end
 
-  def delete_zabbix_server_path(zabbix_server)
-    return nil unless Pundit.policy(current_user, zabbix_server).destroy?
+  def delete_zabbix_server_path(zabbix_server, user: current_user)
+    return nil unless Pundit.policy(user, zabbix_server).destroy?
     # Exclude the default zabbix server from deletion
     return nil if zabbix_server.id == 1
     return zabbix_server_path(zabbix_server)
