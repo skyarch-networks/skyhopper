deef05c09d3ec192e69a4e9aa8f72bb05ae01f9d
Joeper214 <joeper.serrano@gmail.com>
2017-02-03 18:12:05 +0900

    Update marketplace version 2017.02

--- config/application.rb
+++ config/application.rb
@@ -50,7 +50,7 @@ module SkyHopper
     config.active_job.queue_adapter = :sidekiq
 
     # Version information
-    config.my_version = 'Version Marketplace 2016.12'
+    config.my_version = 'Marketplace Version 2017.02'
 
     config.browserify_rails.paths << /frontend\//
   end
