9de620b8fc7d7117f7ec6068abf3579064aa5ff2
Joeper214 <joeper.serrano@gmail.com>
2017-03-27 17:36:08 +0900

    Added radio button vue reusable component

---
+++ app/assets/javascripts/vue_components/radio_button.js
@@ -0,0 +1,22 @@
+exports.install = function(Vue) {
+    Vue.component('radio-button', {
+        props: ['name', 'label', 'choices', 'value'],
+        template: `
+        <label class="radio">
+            <input type="radio" :value="choices" :name="name" v-model="radioButtonValue">
+            <span>{{ label }}</span>
+        </label>
+        `,
+        computed: {
+            radioButtonValue: {
+                get: function () {
+                    return this.value
+                },
+                set: function () {
+                    // Communicate the change to parent component so that selectedValue can be updated
+                    this.$emit("change", this.choices)
+                }
+            }
+        }
+    });
+};
