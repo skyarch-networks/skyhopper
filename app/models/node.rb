71ef41f4487731361f64461b6d0adddff70016a0
candy <lovingrootbeer@gmail.com>
2018-07-05 17:47:57 +0900

    Use string interpolation instead of + method

--- app/models/node.rb
+++ app/models/node.rb
@@ -346,7 +346,7 @@ class Node
 
     case result[:status_text]
       when 'pending'
-        result[:message] = result[:examples].select{|x| x[:status] == 'pending'}.map{|x| x[:full_description]+"\n"+x[:pending_message]}.join("\n")
+        result[:message] = result[:examples].select{|x| x[:status] == 'pending'}.map{|x| "#{x[:full_description]}\n#{x[:pending_message]}"}.join("\n")
         result[:short_msg] = result[:examples].select{|x| x[:status] == 'failed'}.map{|x| x[:full_description]}.join("\n")
       when 'failed'
         result[:message] = result[:examples].select{|x| x[:status] == 'failed'}.map{|x| "#{x[:full_description]}\n#{x[:command]}¥n#{x[:exception][:message]}"}.join("\n")
