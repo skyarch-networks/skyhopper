c2bb21568ec2fff02db31730df47d5ee303f6a55
skyhashiguchi <12824078+skyhashiguchi@users.noreply.github.com>
2018-08-24 15:02:35 +0900

    Update: db_crypt_secretを初回取得時に生成するように変更

--- config/initializers/secrets.rb
+++
@@ -1,6 +0,0 @@
-SkyHopper::Application.secrets[:db_crypt_secret] = Proc.new {
-  db_crypt_key = SkyHopper::Application.secrets[:db_crypt_key]
-  db_crypt_salt = SkyHopper::Application.secrets[:db_crypt_salt]
-  key_len = ActiveSupport::MessageEncryptor.key_len
-  ActiveSupport::KeyGenerator.new(db_crypt_key).generate_key(db_crypt_salt, key_len)
-}.call
