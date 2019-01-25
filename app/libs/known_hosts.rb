0fcaee8094ee97292004880e4fc6110b34d9dd3c
skyhashiguchi <12824078+skyhashiguchi@users.noreply.github.com>
2019-01-25 16:22:33 +0900

    Update: KnownHostsモジュールの処理を変更

--- app/libs/known_hosts.rb
+++ app/libs/known_hosts.rb
@@ -1,30 +1,27 @@
-require 'shellwords'
+require 'net/ssh'
 require 'open3'
 
 module KnownHosts
   class CommandNotSuccessError < ::RuntimeError; end
-  class AnalysisNotSuccessError < ::RuntimeError; end
 
-  def self.exec_command(command)
-    out, err, status = Open3.capture3(command)
-    raise CommandNotSuccessError unless status.success?
-    return out
-  end
+  class << self
+    def exist?(domain_name)
+      return false if domain_name.empty?
+      found_known_hosts = ::Net::SSH::KnownHosts.search_for(domain_name)
+      return !found_known_hosts.empty?
+    end
 
-  def self.exist?(domain_name)
-    command  = "ssh-keygen -F #{Shellwords.escape(domain_name)}"
-    result = exec_command(command)
+    def scan_and_add_keys(domain_name)
+      command  = "ssh-keyscan -H #{Shellwords.escape(domain_name)}>> ~/.ssh/known_hosts"
+      exec_command(command)
+    end
 
-    # 空文字列が返ってきた場合は見つからなかったと判断する
-    return false if result == ''
+    private
 
-    # 見つかったという内容のメッセージが返ってきた場合は見つかったと判断する
-    exist_text_regex_string = "Host #{Regexp.escape(domain_name)} found: line \\d+"
-    exist_text_regex = Regexp.new(exist_text_regex_string)
-    match = exist_text_regex.match(result)
-    return true if match
-
-    # コマンドが想定外の値を返した場合は例外を投げる
-    raise AnalysisNotSuccessError
+    def exec_command(command)
+      out, err, status = Open3.capture3(command)
+      raise CommandNotSuccessError unless status.success?
+      return out
+    end
   end
 end
