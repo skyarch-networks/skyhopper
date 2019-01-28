ba5233d1d34c77efcd42703f6f6138f5bec5b82d
skyhashiguchi <12824078+skyhashiguchi@users.noreply.github.com>
2019-01-28 16:02:29 +0900

    Update: KnownHostsモジュールの処理を変更

--- app/libs/known_hosts.rb
+++ app/libs/known_hosts.rb
@@ -5,18 +5,22 @@ module KnownHosts
   class CommandNotSuccessError < ::RuntimeError; end
 
   class << self
-    def exist?(domain_name)
-      return false if domain_name.empty?
-      found_known_hosts = ::Net::SSH::KnownHosts.search_for(domain_name)
-      return !found_known_hosts.empty?
-    end
-
     def scan_and_add_keys(domain_name)
+      init_ssh_dir
+
       command  = "ssh-keyscan -H #{Shellwords.escape(domain_name)}>> ~/.ssh/known_hosts"
       exec_command(command)
     end
 
     private
+
+    def init_ssh_dir
+      ssh_dir = File.expand_path('~/.ssh')
+      Dir.mkdir(ssh_dir, perm: 0700) unless Dir.exist?(ssh_dir)
+
+      known_hosts_path = File.expand_path('~/.ssh/known_hosts')
+      File.open(known_hosts_path, mode: 'w', perm: 0600).close() unless File.exist?(known_hosts_path)
+    end
 
     def exec_command(command)
       out, err, status = Open3.capture3(command)
