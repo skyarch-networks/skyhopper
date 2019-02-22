07c2b563806afac31cf6b1b2b9e7b7ab56c31ac1
thayanne0621 <r144@skyarch.net>
2019-02-22 17:43:20 +0900

    Update: dishファイルのリファクタリング

--- app/assets/javascripts/models/dish.js
+++ app/assets/javascripts/models/dish.js
@@ -1,24 +1,18 @@
-"use strict";
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+const ModelBase = require('./base').default;
+
+const Dish = class Dish extends ModelBase {
+  constructor(id) {
+    super();
+    this.id = id;
+    this.ajax = new AjaxSet.Resources('dishes');
+    this.ajax.add_member('runlist', 'GET');
+  }
+
+  runlist(id) {
+    return this.WrapAndResolveReject(
+      () => this.ajax.runlist({ id }),
+    );
+  }
 };
-var base_1 = require('./base');
-var Dish = (function (_super) {
-    __extends(Dish, _super);
-    function Dish(id) {
-        _super.call(this);
-        this.id = id;
-    }
-    Dish.prototype.runlist = function (id) {
-        return this.WrapAndResolveReject(function () {
-            return Dish.ajax.runlist({ id: id });
-        });
-    };
-    Dish.ajax = new AjaxSet.Resources('dishes');
-    return Dish;
-}(base_1.default));
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.default = Dish;
-Dish.ajax.add_member('runlist', 'GET');
