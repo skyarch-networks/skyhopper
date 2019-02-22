c8d082f389fc77c5f3e25813f092f102a0218213
thayanne0621 <r144@skyarch.net>
2019-02-22 11:21:02 +0900

    Update: S3バケットとCFTemplateのリファクタリング

--- app/assets/javascripts/models/s3_bucket.js
+++ app/assets/javascripts/models/s3_bucket.js
@@ -4,15 +4,15 @@ const S3Bucket = class S3Bucket extends ModelBase {
   constructor(infra, physicalId) {
     super();
     this.infra = infra;
+    this.ajax = new AjaxSet.Resources('infrastructures');
     this.ajax.add_member('show_s3', 'GET');
     this.physical_id = physicalId;
-    this.ajax = new AjaxSet.Resources('infrastructures');
   }
 
   show() {
     const self = this;
     return this.WrapAndResolveReject(
-      () => S3Bucket.ajax.show_s3({
+      () => self.ajax.show_s3({
         bucket_name: self.physical_id,
         id: self.infra.id,
       }),
