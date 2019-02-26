ef191cb90eafa7439440ffd069b1de4089060e2b
thayanne0621 <r144@skyarch.net>
2019-02-26 13:21:14 +0900

    Update: スナップショットのリファクタリング

--- app/assets/javascripts/models/snapshot.js
+++ app/assets/javascripts/models/snapshot.js
@@ -1,90 +1,89 @@
-"use strict";
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
-var base_1 = require('./base');
-var Snapshot = (function (_super) {
-    __extends(Snapshot, _super);
-    function Snapshot(infra_id) {
-        _super.call(this);
-        this.infra_id = infra_id;
-    }
-    Snapshot.prototype.index = function (volume_id) {
-        var _this = this;
-        return this.WrapAndResolveReject(function () {
-            return Snapshot.ajax.index({
-                infra_id: _this.infra_id,
-                volume_id: volume_id
-            });
-        });
-    };
-    Snapshot.prototype.watch_snapshot_progress = function (dfd) {
-        return function (data) {
-            var ws = ws_connector('snapshot_status', data.snapshot_id);
-            ws.onmessage = function (msg) {
-                switch (msg.data) {
-                    case "completed":
-                        dfd.resolve(data);
-                        ws.close();
-                        break;
-                    default:
-                        dfd.reject(msg.data);
-                        ws.close();
-                        break;
-                }
-            };
-        };
-    };
-    Snapshot.prototype.create = function (volume_id, physical_id) {
-        var _this = this;
-        var dfd = $.Deferred();
-        Snapshot.ajax.create({
-            infra_id: this.infra_id,
-            volume_id: volume_id,
-            physical_id: physical_id
-        }).done(function (data) {
-            dfd.notify(data);
-            _this.watch_snapshot_progress(dfd)(data);
-        }).fail(this.rejectF(dfd));
-        return dfd.promise();
-    };
-    Snapshot.prototype.destroy = function (snapshot_id) {
-        var _this = this;
-        return this.WrapAndResolveReject(function () {
-            return Snapshot.ajax.destroy({
-                infra_id: _this.infra_id,
-                id: snapshot_id
-            });
-        });
-    };
-    Snapshot.prototype.schedule = function (volume_id, physical_id, schedule) {
-        var _this = this;
-        return this.WrapAndResolveReject(function () {
-            return Snapshot.ajax.schedule({
-                infra_id: _this.infra_id,
-                volume_id: volume_id,
-                physical_id: physical_id,
-                schedule: schedule
-            });
-        });
-    };
-    Snapshot.prototype.save_retention_policy = function (volume_id, enabled, max_amount) {
-        var _this = this;
-        return this.WrapAndResolveReject(function () {
-            return Snapshot.ajax.save_retention_policy({
-                infra_id: _this.infra_id,
-                volume_id: volume_id,
-                enabled: enabled,
-                max_amount: max_amount
-            });
-        });
-    };
+const ModelBase = require('./base').default;
+
+const Snapshot = class Snapshot extends ModelBase {
+  constructor(infraId) {
+    super();
+    this.infra_id = infraId;
     Snapshot.ajax = new AjaxSet.Resources('snapshots');
-    return Snapshot;
-}(base_1.default));
-Object.defineProperty(exports, "__esModule", { value: true });
+    Snapshot.ajax.add_collection('schedule', 'POST');
+    Snapshot.ajax.add_collection('save_retention_policy', 'POST');
+  }
+
+  index(volumeId) {
+    const self = this;
+    return this.WrapAndResolveReject(
+      () => Snapshot.ajax.index({
+        infra_id: self.infra_id,
+        volume_id: volumeId,
+      }),
+    );
+  }
+
+  watch_snapshot_progress(dfd) {
+    return (data) => {
+      const ws = ws_connector('snapshot_status', data.snapshot_id);
+      ws.onmessage = (msg) => {
+        switch (msg.data) {
+          case 'completed':
+            dfd.resolve(data);
+            ws.close();
+            break;
+          default:
+            dfd.reject(msg.data);
+            ws.close();
+            break;
+        }
+      };
+    };
+  }
+
+  create(volumeId, physicalId) {
+    const self = this;
+    const dfd = $.Deferred();
+    Snapshot.ajax.create({
+      infra_id: this.infra_id,
+      volume_id: volumeId,
+      physical_id: physicalId,
+    }).done((data) => {
+      dfd.notify(data);
+      self.watch_snapshot_progress(dfd)(data);
+    }).fail(this.rejectF(dfd));
+    return dfd.promise();
+  }
+
+  destroy(snapshotId) {
+    const self = this;
+    return this.WrapAndResolveReject(
+      () => Snapshot.ajax.destroy({
+        infra_id: self.infra_id,
+        id: snapshotId,
+      }),
+    );
+  }
+
+  schedule(volumeId, physicalId, schedule) {
+    const self = this;
+    return this.WrapAndResolveReject(
+      () => Snapshot.ajax.schedule({
+        infra_id: self.infra_id,
+        volume_id: volumeId,
+        physical_id: physicalId,
+        schedule,
+      }),
+    );
+  }
+
+  save_retention_policy(volumeId, enabled, maxAmount) {
+    const self = this;
+    return this.WrapAndResolveReject(
+      () => Snapshot.ajax.save_retention_policy({
+        infra_id: self.infra_id,
+        volume_id: volumeId,
+        enabled,
+        max_amount: maxAmount,
+      }),
+    );
+  }
+};
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.default = Snapshot;
-Snapshot.ajax.add_collection('schedule', 'POST');
-Snapshot.ajax.add_collection('save_retention_policy', 'POST');
