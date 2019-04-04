d294ece7b8ebd29a2b5e7f9780852c4f10a6b1d8
hiyuki2578 <tomoka319@gmail.com>
2019-04-04 18:07:30 +0900

    [WIP] インフラストラクチャの画面分終わらせた

--- app/assets/javascripts/infrastructures/elb-tabpane.js
+++ app/assets/javascripts/infrastructures/elb-tabpane.js
@@ -1,17 +1,19 @@
-var modal          = require('modal');
-var Infrastructure = require('models/infrastructure').default;
-var EC2Instance    = require('models/ec2_instance').default;
-var queryString = require('query-string').parse(location.search);
+const modal = require('modal');
+const Infrastructure = require('models/infrastructure').default;
+const EC2Instance = require('models/ec2_instance').default;
+const queryString = require('query-string').parse(location.search);
 
-var helpers = require('infrastructures/helper.js');
-var alert_success        = helpers.alert_success;
-var alert_danger         = helpers.alert_danger;
-var alert_and_show_infra = helpers.alert_and_show_infra;
-var toLocaleString       = helpers.toLocaleString;
+const helpers = require('infrastructures/helper.js');
 
-var methods = require('infrastructures/common-methods');
-var has_selected         = methods.has_selected;
-var check_tag            = methods.check_tag;
+const alert_success = helpers.alert_success;
+const alert_danger = helpers.alert_danger;
+const alert_and_show_infra = helpers.alert_and_show_infra;
+const toLocaleString = helpers.toLocaleString;
+
+const methods = require('infrastructures/common-methods');
+
+const has_selected = methods.has_selected;
+const check_tag = methods.check_tag;
 
 // this.physical_id is a elb_name.
 module.exports = Vue.extend({
@@ -28,43 +30,45 @@ module.exports = Vue.extend({
     },
   },
 
-  data: function () {return {
-    ec2_instances: [],
-    unregistereds: [],
-    lang: queryString.lang,
-    dns_name: "",
-    listeners: [],
-    selected_ec2: null,
-    server_certificates: [],
-    server_certificate_name: '',
-    server_certificate_name_items: [],
-    loading: false,
-    protocol: '',
-    load_balancer_port: '',
-    instance_protocol: '',
-    instance_port: '',
-    ssl_certificate_id: '',
-    rules_summary: null,
-    page: 0,
-    dispItemSize: 10,
-    filteredLength: null,
-    filterKey: '',
-    private_key: '',
-    certificate_body: '',
-    certificate_chain: '',
-    loading_s: false,
-    loading_groups: '',
-  };},
+  data() {
+    return {
+      ec2_instances: [],
+      unregistereds: [],
+      lang: queryString.lang,
+      dns_name: '',
+      listeners: [],
+      selected_ec2: null,
+      server_certificates: [],
+      server_certificate_name: '',
+      server_certificate_name_items: [],
+      loading: false,
+      protocol: '',
+      load_balancer_port: '',
+      instance_protocol: '',
+      instance_port: '',
+      ssl_certificate_id: '',
+      rules_summary: null,
+      page: 0,
+      dispItemSize: 10,
+      filteredLength: null,
+      filterKey: '',
+      private_key: '',
+      certificate_body: '',
+      certificate_chain: '',
+      loading_s: false,
+      loading_groups: '',
+    };
+  },
 
   methods: {
-    show_ec2: function (physical_id) { this.$parent.show_ec2(physical_id); },
+    show_ec2(physical_id) { this.$parent.show_ec2(physical_id); },
 
-    deregister: function (physical_id) {
-      var self = this;
-      modal.Confirm(t('infrastructures.infrastructure'), t('ec2_instances.confirm.deregister'), 'danger').done(function () {
-        var infra = new Infrastructure(self.infra_id);
-        var ec2 = new EC2Instance(infra, physical_id);
-        var reload = function () {
+    deregister(physical_id) {
+      const self = this;
+      modal.Confirm(t('infrastructures.infrastructure'), t('ec2_instances.confirm.deregister'), 'danger').done(() => {
+        const infra = new Infrastructure(self.infra_id);
+        const ec2 = new EC2Instance(infra, physical_id);
+        const reload = function () {
           self.$parent.show_elb(self.physical_id);
         };
         ec2.deregister(self.physical_id)
@@ -73,12 +77,12 @@ module.exports = Vue.extend({
       });
     },
 
-    register: function () {
-      var self = this;
-      modal.Confirm(t('infrastructures.infrastructure'), t('ec2_instances.confirm.register')).done(function () {
-        var infra = new Infrastructure(self.infra_id);
-        var ec2 = new EC2Instance(infra, self.selected_ec2);
-        var reload = function () {
+    register() {
+      const self = this;
+      modal.Confirm(t('infrastructures.infrastructure'), t('ec2_instances.confirm.register')).done(() => {
+        const infra = new Infrastructure(self.infra_id);
+        const ec2 = new EC2Instance(infra, self.selected_ec2);
+        const reload = function () {
           self.$parent.show_elb(self.physical_id);
         };
         ec2.register(self.physical_id)
@@ -87,98 +91,98 @@ module.exports = Vue.extend({
       });
     },
 
-    state: function (s){ return s === 'InService' ? 'success' : 'danger'; },
+    state(s) { return s === 'InService' ? 'success' : 'danger'; },
 
-    ssl_certificate_id_to_name: function (ssl_certificate_id) {
+    ssl_certificate_id_to_name(ssl_certificate_id) {
       if (!ssl_certificate_id) {
-        return "";
-      } else if (ssl_certificate_id === "Invalid-Certificate"){
-        return "Invalid-Certificate";
+        return '';
+      } if (ssl_certificate_id === 'Invalid-Certificate') {
+        return 'Invalid-Certificate';
       }
-      return ssl_certificate_id.replace(/arn:aws:iam::[0-9]+:server-certificate\//, "");
+      return ssl_certificate_id.replace(/arn:aws:iam::[0-9]+:server-certificate\//, '');
     },
 
-    expiration_date: function (date_str) {
-      if (!date_str) { return ""; }
+    expiration_date(date_str) {
+      if (!date_str) { return ''; }
 
       return toLocaleString(date_str);
     },
 
-    set_create_listener_modal_default_value: function (){
-      var self = this;
-      self.protocol = "";
-      self.load_balancer_port = "";
-      self.instance_protocol = "";
-      self.instance_port = "";
-      self.ssl_certificate_id = "";
+    set_create_listener_modal_default_value() {
+      const self = this;
+      self.protocol = '';
+      self.load_balancer_port = '';
+      self.instance_protocol = '';
+      self.instance_port = '';
+      self.ssl_certificate_id = '';
     },
 
-    set_edit_listener_modal_default_value: function (protocol, load_balancer_port, instance_protocol, instance_port, ssl_certificate_id){
-      var self = this;
+    set_edit_listener_modal_default_value(protocol, load_balancer_port, instance_protocol, instance_port, ssl_certificate_id) {
+      const self = this;
       self.old_load_balancer_port = load_balancer_port;
       self.protocol = protocol;
       self.load_balancer_port = load_balancer_port;
       self.instance_protocol = instance_protocol;
       self.instance_port = instance_port;
-      if (ssl_certificate_id === "Invalid-Certificate"){
-        self.ssl_certificate_id = "";
+      if (ssl_certificate_id === 'Invalid-Certificate') {
+        self.ssl_certificate_id = '';
       } else {
         self.ssl_certificate_id = ssl_certificate_id;
       }
     },
 
-    change_listener_protocol: function(){
-      var self = this;
-      if (self.protocol !== "HTTPS" && self.protocol !== "SSL") {
-        self.ssl_certificate_id = "";
+    change_listener_protocol() {
+      const self = this;
+      if (self.protocol !== 'HTTPS' && self.protocol !== 'SSL') {
+        self.ssl_certificate_id = '';
       }
     },
 
-    create_listener: function(){
-      var self = this;
+    create_listener() {
+      const self = this;
       self.loading = true;
-      var infra = new Infrastructure(self.infra_id);
-      var ec2 = new EC2Instance(infra, "");
-      var reload = function () {
+      const infra = new Infrastructure(self.infra_id);
+      const ec2 = new EC2Instance(infra, '');
+      const reload = function () {
         self.$parent.show_elb(self.physical_id);
       };
       ec2.create_listener(self.physical_id, self.protocol, self.load_balancer_port, self.instance_protocol, self.instance_port, self.ssl_certificate_id)
-        .done(function (msg) {
+        .done((msg) => {
           alert_success(reload)(msg);
           $('#create-listener-modal').modal('hide');
         })
-        .fail(function (msg) {
+        .fail((msg) => {
           alert_danger(reload)(msg);
           $('#create-listener-modal').modal('hide');
         });
     },
 
-    update_listener: function(){
-      var self = this;
+    update_listener() {
+      const self = this;
       self.loading = true;
-      var infra = new Infrastructure(self.infra_id);
-      var ec2 = new EC2Instance(infra, "");
-      var reload = function () {
+      const infra = new Infrastructure(self.infra_id);
+      const ec2 = new EC2Instance(infra, '');
+      const reload = function () {
         self.$parent.show_elb(self.physical_id);
       };
       ec2.update_listener(self.physical_id, self.protocol, self.old_load_balancer_port, self.load_balancer_port, self.instance_protocol, self.instance_port, self.ssl_certificate_id)
-        .done(function (msg) {
+        .done((msg) => {
           alert_success(reload)(msg);
           $('#edit-listener-modal').modal('hide');
         })
-        .fail(function (msg) {
+        .fail((msg) => {
           alert_danger(reload)(msg);
           $('#edit-listener-modal').modal('hide');
         });
     },
 
-    delete_listener: function(load_balancer_port){
-      var self = this;
+    delete_listener(load_balancer_port) {
+      const self = this;
       self.load_balancer_port = load_balancer_port;
-      modal.Confirm(t('ec2_instances.btn.delete_to_elb_listener'), t('ec2_instances.confirm.delete_listener'), 'danger').done(function () {
-        var infra = new Infrastructure(self.infra_id);
-        var ec2 = new EC2Instance(infra, "");
-        var reload = function () {
+      modal.Confirm(t('ec2_instances.btn.delete_to_elb_listener'), t('ec2_instances.confirm.delete_listener'), 'danger').done(() => {
+        const infra = new Infrastructure(self.infra_id);
+        const ec2 = new EC2Instance(infra, '');
+        const reload = function () {
           self.$parent.show_elb(self.physical_id);
         };
         ec2.delete_listener(self.physical_id, self.load_balancer_port)
@@ -187,32 +191,32 @@ module.exports = Vue.extend({
       });
     },
 
-    upload_server_certificate: function(){
-      var self = this;
+    upload_server_certificate() {
+      const self = this;
       self.loading = true;
-      var infra = new Infrastructure(self.infra_id);
-      var ec2 = new EC2Instance(infra, "");
-      var reload = function () {
+      const infra = new Infrastructure(self.infra_id);
+      const ec2 = new EC2Instance(infra, '');
+      const reload = function () {
         self.$parent.show_elb(self.physical_id);
       };
       ec2.upload_server_certificate(self.physical_id, self.server_certificate_name, self.certificate_body, self.private_key, self.certificate_chain)
-        .done(function (msg) {
+        .done((msg) => {
           alert_success(reload)(msg);
           $('#upload-server-certificate-modal').modal('hide');
         })
-        .fail(function (msg) {
+        .fail((msg) => {
           alert_danger(reload)(msg);
           $('#upload-server-certificate-modal').modal('hide');
         });
     },
 
-    delete_server_certificate: function(server_certificate_name){
-      var self = this;
+    delete_server_certificate(server_certificate_name) {
+      const self = this;
       self.server_certificate_name = server_certificate_name;
-      modal.Confirm(t('ec2_instances.btn.delete_certificate'), t('ec2_instances.confirm.delete_certificate'), 'danger').done(function () {
-        var infra = new Infrastructure(self.infra_id);
-        var ec2 = new EC2Instance(infra, "");
-        var reload = function () {
+      modal.Confirm(t('ec2_instances.btn.delete_certificate'), t('ec2_instances.confirm.delete_certificate'), 'danger').done(() => {
+        const infra = new Infrastructure(self.infra_id);
+        const ec2 = new EC2Instance(infra, '');
+        const reload = function () {
           self.$parent.show_elb(self.physical_id);
         };
         ec2.delete_server_certificate(self.physical_id, self.server_certificate_name)
@@ -221,86 +225,77 @@ module.exports = Vue.extend({
       });
     },
 
-    panel_class: function (state) { return 'panel-' + this.state(state);},
-    label_class: function (state) { return 'label-' + this.state(state);},
-    check:       function (i) { i.checked= !i.checked; },
-    reload:      function(){ this.$parent.show_elb(this.physical_id); },
+    panel_class(state) { return `panel-${this.state(state)}`; },
+    label_class(state) { return `label-${this.state(state)}`; },
+    check(i) { i.checked = !i.checked; },
+    reload() { this.$parent.show_elb(this.physical_id); },
 
-    submit_groups: function(){
-      var self = this;
-      var infra = new Infrastructure(self.infra_id);
-      var ec2 = new EC2Instance(infra, '');
-      var group_ids = this.rules_summary.filter(function (t) {
-        return t.checked;
-      }).map(function (t) {
-        return t.group_id;
-      });
-      var reload = function () {
+    submit_groups() {
+      const self = this;
+      const infra = new Infrastructure(self.infra_id);
+      const ec2 = new EC2Instance(infra, '');
+      const group_ids = this.rules_summary.filter(t => t.checked).map(t => t.group_id);
+      const reload = function () {
         self.$parent.show_elb(self.physical_id);
       };
 
       ec2.elb_submit_groups(group_ids, self.physical_id)
         .done(alert_success(reload))
         .fail(alert_danger(reload));
-
     },
-    view_rules: function () {
+    view_rules() {
       this.$parent.tabpaneID = 'view-rules';
       this.$parent.sec_group = this.rules_summary;
       this.$parent.instance_type = 'elb';
     },
-    has_selected: function(arg){
-      if(arg){
-        return arg.some(function(c){
-          return c.checked;
-        });
+    has_selected(arg) {
+      if (arg) {
+        return arg.some(c => c.checked);
       }
     },
-    check_tag: function(r){
+    check_tag(r) {
       check_tag(r);
     },
-    showPrev: function (){
-      if(this.isStartPage) return;
+    showPrev() {
+      if (this.isStartPage) return;
       this.page--;
     },
-    showNext: function (){
-      if(this.isEndPage) return;
+    showNext() {
+      if (this.isEndPage) return;
       this.page++;
     },
-    roundup: function (val) { return (Math.ceil(val));},
+    roundup(val) { return (Math.ceil(val)); },
   },
 
   computed: {
-    dispItems: function(){
-      var startPage = this.page * this.dispItemSize;
-      if (this.filterKey === ''){
+    dispItems() {
+      const startPage = this.page * this.dispItemSize;
+      if (this.filterKey === '') {
         return this.rules_summary.slice(startPage, startPage + this.dispItemSize);
       }
-      else{
-        return this.rules_summary;
-      }
+
+      return this.rules_summary;
     },
-    filterd_dispitems: function(){
-      var self = this;
-      var items = this.dispItems.filter(function (data) {
-        if(self.filterKey === ""){
-          return true
-        } else {
-          return JSON.stringify(data).toLowerCase().indexOf(self.filterKey.toLowerCase()) !== -1;
+    filterd_dispitems() {
+      const self = this;
+      const items = this.dispItems.filter((data) => {
+        if (self.filterKey === '') {
+          return true;
         }
+        return JSON.stringify(data).toLowerCase().indexOf(self.filterKey.toLowerCase()) !== -1;
       });
       self.filteredLength = items.length;
       return items;
     },
-    isStartPage: function(){ return (this.page === 0); },
-    isEndPage: function(){ return ((this.page + 1) * this.dispItemSize >= this.rules_summary.length); }
+    isStartPage() { return (this.page === 0); },
+    isEndPage() { return ((this.page + 1) * this.dispItemSize >= this.rules_summary.length); },
   },
 
-  mounted: function () {
+  mounted() {
     this.$nextTick(function () {
-      var self = this;
-      var infra = new Infrastructure(self.infra_id);
-      infra.show_elb(this.physical_id).done(function (data) {
+      const self = this;
+      const infra = new Infrastructure(self.infra_id);
+      infra.show_elb(this.physical_id).done((data) => {
         self.ec2_instances = data.ec2_instances;
         self.unregistereds = data.unregistereds;
         self.dns_name = data.dns_name;
@@ -312,11 +307,11 @@ module.exports = Vue.extend({
         self.$parent.loading = false;
         console.log(self);
       }).fail(alert_and_show_infra(infra.id));
-    })
+    });
   },
   filters: {
 
-    count: function (arr) {
+    count(arr) {
       // record length
       this.$set('filteredLength', arr.length);
       // return it intact
--- app/assets/javascripts/infrastructures/show_infra.js
+++ app/assets/javascripts/infrastructures/show_infra.js
@@ -1,8 +1,9 @@
-var queryString = require('query-string');
-var SHOW_INFRA_ID = '#infra-show';
+const queryString = require('query-string');
+
+const SHOW_INFRA_ID = '#infra-show';
 var show_infra;
 
-var initialize = function (show_infra_func) {
+const initialize = function (show_infra_func) {
   show_infra = show_infra_func;
 };
 
@@ -10,15 +11,15 @@ var show_infra = function (infra_id, current_tab) {
   show_infra(infra_id, current_tab);
 };
 
-var reload_infra_index_page = function () {
-  var parsed_query_string  = queryString.parse(location.search);
-  var project_id_query = parsed_query_string.project_id ? '&project_id=' + parsed_query_string.project_id : '';
-  location.href = '/infrastructures?lang=' + parsed_query_string.lang + project_id_query;
+const reload_infra_index_page = function () {
+  const parsed_query_string = queryString.parse(location.search);
+  const project_id_query = parsed_query_string.project_id ? `&project_id=${parsed_query_string.project_id}` : '';
+  location.href = `/infrastructures?lang=${parsed_query_string.lang}${project_id_query}`;
 };
 
 module.exports = {
-  SHOW_INFRA_ID: SHOW_INFRA_ID,
-  initialize: initialize,
-  show_infra: show_infra,
-  reload_infra_index_page: reload_infra_index_page,
+  SHOW_INFRA_ID,
+  initialize,
+  show_infra,
+  reload_infra_index_page,
 };
