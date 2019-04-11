0f49858df92c2701fba853d2bcde2ca6fc3aadbd
rubysukisuki326 <12824078+rubysukisuki326@users.noreply.github.com>
2019-04-11 06:23:21 +0000

    Fix: scripts/app_setup.shの修正

--- scripts/app_setup.sh
+++ scripts/app_setup.sh
@@ -8,7 +8,7 @@ cd frontend/
 ~/.yarn/bin/yarn
 cd ..
 
-echo "===== Install the library ====="
+echo "===== Set up the dummy font ====="
 if [[ ! -e frontend/fonts/fonts_map.js ]]; then
   echo 'module.exports = {"dummy":{"normal":"dummy","bold":"dummy","italics":"dummy","bolditalics":"dummy"}};' > frontend/fonts/fonts_map.js
 fi
