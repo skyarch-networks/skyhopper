f15debe3b77822da603185591c61d5b43a6e3bf6
rubysukisuki326 <12824078+rubysukisuki326@users.noreply.github.com>
2019-05-30 08:51:38 +0000

    Fix: rubocopのエラーを抑制

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -9,7 +9,8 @@
 module FormHelper
   def parts_select(options: nil, selected_option: nil, klass: nil, attributes: nil)
     selected_empty_option = please_select_option(selected_option) if selected_option
-    <<~TEMPLATE.html_safe
+    # TODO html_safeは良くないので直す
+    <<~TEMPLATE.html_safe # rubocop:disable Rails/OutputSafety
       <select class="form-control #{klass}" #{attributes}>
         #{selected_empty_option if selected_option}
         #{options}
@@ -18,7 +19,8 @@ module FormHelper
   end
 
   def parts_input(type: 'text', klass: nil, placeholder: nil, attributes: nil)
-    "<input type=\"#{type}\" class=\"form-control #{klass}\" #{"placeholder=\"#{placeholder}\"" if placeholder} #{attributes}>".html_safe
+    # TODO html_safeは良くないので直す
+    "<input type=\"#{type}\" class=\"form-control #{klass}\" #{"placeholder=\"#{placeholder}\"" if placeholder} #{attributes}>".html_safe # rubocop:disable Rails/OutputSafety
   end
 
   def please_select_option(msg = I18n.t('common.please_select'))
