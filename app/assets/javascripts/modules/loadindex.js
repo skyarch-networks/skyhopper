c4d79309a18d614e07879741198628d8fda57b26
rubysukisuki326 <12824078+rubysukisuki326@users.noreply.github.com>
2019-06-11 07:17:11 +0000

    Fix: odules/loadindex.jsをリファクタリング

--- app/assets/javascripts/modules/loadindex.js
+++ app/assets/javascripts/modules/loadindex.js
@@ -6,13 +6,11 @@
 // http://opensource.org/licenses/mit-license.php
 //
 
-module.exports = function () {
-  return new Vue({
-    el: '#indexElement',
-    data: {
-      searchQuery: '',
-      gridColumns: [], // ['stack_name', 'region', 'keypairname', 'created_at', 'status', 'id'],
-      gridData: [],
-    },
-  });
-};
+module.exports = () => new Vue({
+  el: '#indexElement',
+  data: {
+    searchQuery: '',
+    gridColumns: [], // ['stack_name', 'region', 'keypairname', 'created_at', 'status', 'id'],
+    gridData: [],
+  },
+});
