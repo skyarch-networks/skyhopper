825674bb15cc309349de8356fd534feff3b44190
rubysukisuki326 <12824078+rubysukisuki326@users.noreply.github.com>
2019-06-11 07:11:29 +0000

    Fix: modules/listen.jsをリファクタリング

--- app/assets/javascripts/modules/listen.js
+++ app/assets/javascripts/modules/listen.js
@@ -5,34 +5,10 @@
 //
 // http://opensource.org/licenses/mit-license.php
 //
-(function () {
+(() => {
   'use_strict';
 
-  module.exports = function (value, key, index, lang) {
-    switch (index) {
-      case 'infrastructures':
-        return render_infrastructures(value, key, lang);
-      case 'projects':
-        return render_projects(value, key, lang);
-      case 'clients':
-        return render_clients(value, key, lang);
-      case 'serverspecs':
-        return render_serverspecs(value, key, lang);
-      case 'dishes':
-        return render_dish(value, key, lang);
-      case 'cf_templates':
-        return render_cf_templates(value, key, lang);
-      case 'user_admin':
-        return render_user_admin(value, key, lang);
-      case 'servertest_results':
-        return render_servertests_results(value, key, lang);
-      default:
-        return value;
-    }
-  };
-
-
-  function render_infrastructures(value, key, lang) {
+  function renderInfrastructures(value, key) {
     const SUCCESS_STATUSES = [
       'CREATE_COMPLETE', 'UPDATE_COMPLETE', 'DELETE_COMPLETE',
     ];
@@ -62,29 +38,30 @@
     return value;
   }
 
-  function render_clients(value, key, lang) {
+  function renderClients(value, key) {
     if (key === 'code') {
       return `${value[0]} <span class='label label-success'>${value[1]} ${t('clients.projects')}</span>`;
     }
     return value;
   }
 
-  function render_projects(value, key, lang) {
+  function renderProjects(value, key) {
     if (key === 'code') {
       return `${value[0]} <span class='label label-success'>${value[1]} ${t('projects.infras')}</span>`;
     }
     return value;
   }
 
-  function render_serverspecs(value, key, lang) {
+  function renderServerspecs(value, key) {
     return (key !== 'serverspec_name') ? value : '';
   }
 
-  function render_cf_templates(value, key, lang) {
+  function renderCfTemplates(value, key) {
     return (key !== 'cf_subject') ? value : '';
   }
 
-  function render_dish(value, key, lang) {
+  function renderDish(_value, key) {
+    let value = _value;
     if (key === 'status') {
       let label = null;
       if (value === 'SUCCESS') {
@@ -98,13 +75,14 @@
         value = 'NOT YET';
       }
       return `<span class='label ${label}'>${value}</span>`;
-    } if (key === 'dish_name') {
+    }
+    if (key === 'dish_name') {
       return '';
     }
     return value;
   }
 
-  function render_user_admin(value, key, lang) {
+  function renderUserAdmin(value, key) {
     if (key === 'role') {
       return `${value[0]}&nbsp${value[1]}`;
     } if (key === 'email') {
@@ -113,7 +91,7 @@
     return value;
   }
 
-  function render_servertests_results(value, key) {
+  function renderServertestsResults(value, key) {
     switch (key) {
       case 'status':
         if (value === 'success') {
@@ -122,30 +100,33 @@
           return `<span class='label label-danger'>${value}</span>`;
         } if (value === 'pending') {
           return `<span class='label label-warning'>${value}</span>`;
-        } return value;
-        break;
-      case 'servertest':
-        var names = value.servertests.map(val => val.name);
+        }
+        return value;
+      case 'servertest': {
+        const names = value.servertests.map(val => val.name);
         if (value.auto_generated === true) {
           names.push('auto generated');
         } else if (value.auto_generated === null) {
           names.push('(auto generated servertest may have been executed)');
         }
         return names.join(', ');
-      case 'category':
-        var category = [];
+      }
+      case 'category': {
+        const category = [];
         value.forEach((argument) => {
           if (!category.includes(argument.category)) category.push(argument.category);
         });
 
         return (value.length > 0) ? category : 'serverspec';
+      }
 
-      case 'message':
+      case 'message': {
         if (value.servertest_result_details.length <= 0 && value.auto_generated_servertest === false) {
           return `<span class='text text-success'> serverspec for ${value.physical_id} is successfully finished. </span>`;
         }
-        var head = `<td>Serverspec for ${value.physical_id} <a href='#' data-toggle='collapse' data-target='#logbody-${value.id}' class='accordion-toggle popovermore'> ... <span class='glyphicon glyphicon-zoom-in'></span></a></td>`;
-        var body = '';
+        // eslint-disable-next-line max-len
+        const head = `<td>Serverspec for ${value.physical_id} <a href='#' data-toggle='collapse' data-target='#logbody-${value.id}' class='accordion-toggle popovermore'> ... <span class='glyphicon glyphicon-zoom-in'></span></a></td>`;
+        let body = '';
         if (value.message) {
           body = `${"<div class='col-sm-12'>"
             + "<td class='hidden-row'>"
@@ -156,9 +137,32 @@
             + '</div>';
         }
         return head + body;
-        break;
+      }
       default:
         return value;
     }
   }
-}());
+
+  module.exports = (value, key, index, lang) => {
+    switch (index) {
+      case 'infrastructures':
+        return renderInfrastructures(value, key, lang);
+      case 'projects':
+        return renderProjects(value, key, lang);
+      case 'clients':
+        return renderClients(value, key, lang);
+      case 'serverspecs':
+        return renderServerspecs(value, key, lang);
+      case 'dishes':
+        return renderDish(value, key, lang);
+      case 'cf_templates':
+        return renderCfTemplates(value, key, lang);
+      case 'user_admin':
+        return renderUserAdmin(value, key, lang);
+      case 'servertest_results':
+        return renderServertestsResults(value, key, lang);
+      default:
+        return value;
+    }
+  };
+})();
