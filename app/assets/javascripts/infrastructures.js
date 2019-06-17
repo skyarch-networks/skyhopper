90ceaccf0383a24980f44f99a880648650cacb30
rubysukisuki326 <12824078+rubysukisuki326@users.noreply.github.com>
2019-06-17 03:13:05 +0000

    Fix: infrastructures.jsをリファクタリング

--- app/assets/javascripts/infrastructures.js
+++ app/assets/javascripts/infrastructures.js
@@ -6,120 +6,144 @@
 // http://opensource.org/licenses/mit-license.php
 //
 
+const queryString = require('query-string').parse(window.location.search);
+const Infrastructure = require('./models/infrastructure').default;
+const modal = require('./modal');
+const datepicker = require('./modules/datepicker');
+const vace = require('./modules/vue-ace');
+require('brace/mode/json');
+require('brace/mode/yaml');
+require('brace/theme/github');
+const show = require('./infrastructures/show_infra.js');
+const newVM = require('./modules/newVM');
 
-(function () {
+// Vue component
+const stackEventsTable = require('./infrastructures/stack-events-table.js');
+const addModifyTabpane = require('./infrastructures/add-modify-tabpane.js');
+const insertCfParams = require('./infrastructures/insert-cf-params.js');
+const addEc2Tabpane = require('./infrastructures/add-ec2-tabpane.js');
+const cfHistoryTabpane = require('./infrastructures/cf-history-tabpane.js');
+const infraLogsTabpane = require('./infrastructures/infra-logs-tabpane.js');
+const monitoringTabpane = require('./infrastructures/monitoring-tabpane.js');
+const editMonitoringTabpane = require('./infrastructures/edit-monitoring-tabpane.js');
+const rdsTabpane = require('./infrastructures/rds-tabpane.js');
+const elbTabpane = require('./infrastructures/elb-tabpane.js');
+const s3Tabpane = require('./infrastructures/s3-tabpane.js');
+const viewRulesTabpane = require('./infrastructures/view-rules-tabpane.js');
+const securityGroupsTabpane = require('./infrastructures/security-groups-tabpane.js');
+const ec2Tabpane = require('./infrastructures/ec2-tabpane.js');
+const servertestResultsTabpane = require('./infrastructures/servertest-results-tabpane.js');
+const serverspecTabpane = require('./infrastructures/serverspec-tabpane.js');
+const operationSchedTabpane = require('./infrastructures/operation-sched-tabpane.js');
+const editAnsiblePlaybookTabpane = require('./infrastructures/edit-ansible-playbook-tabpane.js');
+const demoGrid = require('./demo-grid.js');
+
+{
   // ================================================================
   // infrastructures
   // ================================================================
 
   // browserify functions for vue filters functionality
   // var infraindex = require('./modules/loadindex');
-  const queryString = require('query-string').parse(location.search);
-  // browserify modules for Vue directives
-  const Infrastructure = require('models/infrastructure').default;
-  const modal = require('modal');
 
-  Vue.use(require('./modules/datepicker'), queryString.lang);
+  Vue.use(datepicker, queryString.lang);
 
-  const vace = require('./modules/vue-ace');
-  require('brace/mode/json');
-  require('brace/mode/yaml');
-  require('brace/theme/github');
+  Vue.component('stack-events-table', stackEventsTable);
+  Vue.component('add-modify-tabpane', addModifyTabpane);
+  Vue.component('insert-cf-params', insertCfParams);
+  Vue.component('add-ec2-tabpane', addEc2Tabpane);
+  Vue.component('cf-history-tabpane', cfHistoryTabpane);
+  Vue.component('infra-logs-tabpane', infraLogsTabpane);
+  Vue.component('monitoring-tabpane', monitoringTabpane);
+  Vue.component('edit-monitoring-tabpane', editMonitoringTabpane);
+  Vue.component('rds-tabpane', rdsTabpane);
+  Vue.component('elb-tabpane', elbTabpane);
+  Vue.component('s3-tabpane', s3Tabpane);
+  Vue.component('view-rules-tabpane', viewRulesTabpane);
+  Vue.component('security-groups-tabpane', securityGroupsTabpane);
+  Vue.component('ec2-tabpane', ec2Tabpane);
+  Vue.component('servertest-results-tabpane', servertestResultsTabpane);
+  Vue.component('serverspec-tabpane', serverspecTabpane);
+  Vue.component('operation-sched-tabpane', operationSchedTabpane);
+  Vue.component('edit-ansible-playbook-tabpane', editAnsiblePlaybookTabpane);
+  Vue.component('demo-grid', demoGrid);
 
+  const showInfraInitialize = show.initialize;
+  const showInfra = show.show_infra;
+  const { SHOW_INFRA_ID } = show;
+  const reloadInfraIndexPage = show.reload_infra_index_page;
 
-  Vue.component('stack-events-table', require('infrastructures/stack-events-table.js'));
-  Vue.component('add-modify-tabpane', require('infrastructures/add-modify-tabpane.js'));
-  Vue.component('insert-cf-params', require('infrastructures/insert-cf-params.js'));
-  Vue.component('add-ec2-tabpane', require('infrastructures/add-ec2-tabpane.js'));
-  Vue.component('cf-history-tabpane', require('infrastructures/cf-history-tabpane.js'));
-  Vue.component('infra-logs-tabpane', require('infrastructures/infra-logs-tabpane.js'));
-  Vue.component('monitoring-tabpane', require('infrastructures/monitoring-tabpane.js'));
-  Vue.component('edit-monitoring-tabpane', require('infrastructures/edit-monitoring-tabpane.js'));
-  Vue.component('rds-tabpane', require('infrastructures/rds-tabpane.js'));
-  Vue.component('elb-tabpane', require('infrastructures/elb-tabpane.js'));
-  Vue.component('s3-tabpane', require('infrastructures/s3-tabpane.js'));
-  Vue.component('view-rules-tabpane', require('infrastructures/view-rules-tabpane.js'));
-  Vue.component('security-groups-tabpane', require('infrastructures/security-groups-tabpane.js'));
-  Vue.component('ec2-tabpane', require('infrastructures/ec2-tabpane.js'));
-  Vue.component('servertest-results-tabpane', require('infrastructures/servertest-results-tabpane.js'));
-  Vue.component('serverspec-tabpane', require('infrastructures/serverspec-tabpane.js'));
-  Vue.component('operation-sched-tabpane', require('infrastructures/operation-sched-tabpane.js'));
-  Vue.component('edit-ansible-playbook-tabpane', require('infrastructures/edit-ansible-playbook-tabpane.js'));
-  Vue.component('demo-grid', require('demo-grid.js'));
-
-
-  const show = require('infrastructures/show_infra.js');
-  const show_infra_initialize = show.initialize;
-  const show_infra = show.show_infra;
-  const SHOW_INFRA_ID = show.SHOW_INFRA_ID;
-  const reload_infra_index_page = show.reload_infra_index_page;
-
-  const detach = function (infra_id) {
+  const detach = (infraId) => {
     modal.Confirm(t('infrastructures.infrastructure'), t('infrastructures.msg.detach_stack_confirm'), 'danger').done(() => {
-      const infra = new Infrastructure(infra_id);
+      const infra = new Infrastructure(infraId);
       const l = new Loader();
       l.text = 'Loading...';
       l.$mount(SHOW_INFRA_ID);
       infra.detach().done((msg) => {
         modal.Alert(t('infrastructures.infrastructure'), msg).done(() => {
-          reload_infra_index_page();
+          reloadInfraIndexPage();
         });
       }).fail(modal.AlertForAjaxStdError()).always(l.$destroy);
     });
   };
 
-  const delete_stack = function (infra_id) {
+  const deleteStack = (infraId) => {
     modal.Confirm(t('infrastructures.infrastructure'), t('infrastructures.msg.delete_stack_confirm'), 'danger').done(() => {
-      const infra = new Infrastructure(infra_id);
+      const infra = new Infrastructure(infraId);
       const l = new Loader();
       l.text = 'Loading...';
       l.$mount(SHOW_INFRA_ID);
       infra.delete_stack().done((msg) => {
         modal.Alert(t('infrastructures.infrastructure'), msg).done(() => {
-          show_infra(infra_id);
+          showInfra(infraId);
         });
         // TODO: reload
       }).fail(modal.AlertForAjaxStdError(() => {
-        show_infra(infra_id);
+        showInfra(infraId);
       })).always(l.$destroy);
     });
   };
 
 
   // for infrastructures#new
-  const new_ec2_key = function () {
-    const region_input = $('#infrastructure_region');
-    const region = region_input.val();
-    const project_id = $('#infrastructure_project_id').val();
-    let name_file;
-    modal.Confirm(t('infrastructures.infrastructure'), t('ec2_private_keys.confirm.create')).then(() => modal.Prompt(t('infrastructures.infrastructure'), t('app_settings.keypair_name'))).then((name) => {
+  const newEc2Key = () => {
+    const regionInput = $('#infrastructure_region');
+    const region = regionInput.val();
+    const projectId = $('#infrastructure_project_id').val();
+    let nameFile;
+    modal.Confirm(
+      t('infrastructures.infrastructure'),
+      t('ec2_private_keys.confirm.create'),
+    ).then(
+      () => modal.Prompt(t('infrastructures.infrastructure'), t('app_settings.keypair_name')),
+    ).then((name) => {
       if (!name) {
         modal.Alert(t('infrastructures.infrastructure'), t('ec2_private_keys.msg.please_name'), 'danger');
         return;
       }
 
-      name_file = name;
+      nameFile = name;
       return $.ajax({
         url: '/ec2_private_keys',
         type: 'POST',
         data: {
           name,
           region,
-          project_id,
+          projectId,
         },
       });
     }).done((key) => {
-      const value = key.value;
+      const { value } = key;
       const textarea = $('#keypair_value');
-      const keypair_name = $('#keypair_name');
+      const keypairName = $('#keypair_name');
       textarea.val(value);
-      keypair_name.val(name_file);
+      keypairName.val(nameFile);
       textarea.attr('readonly', true);
-      keypair_name.attr('readonly', true);
-      region_input.attr('readonly', true);
+      keypairName.attr('readonly', true);
+      regionInput.attr('readonly', true);
 
       // download file.
-      const file = new File([value], `${name_file}.pem`);
+      const file = new File([value], `${nameFile}.pem`);
       const url = window.URL.createObjectURL(file);
       const a = document.createElement('a');
       a.href = url;
@@ -137,10 +161,7 @@
   // ================================================================
 
   if ($('#infrastructureApp').length) {
-    const newVM = require('modules/newVM');
-
-
-    const infrastructure_url = queryString.project_id ? `&project_id=${queryString.project_id}` : '';
+    const infrastructureUrl = queryString.project_id ? `&project_id=${queryString.project_id}` : '';
     const index = {
       template: '#index-template',
       replace: true,
@@ -151,7 +172,7 @@
           gridData: [],
           loading: true,
           is_empty: false,
-          url: `infrastructures?lang=${queryString.lang}${infrastructure_url}`,
+          url: `infrastructures?lang=${queryString.lang}${infrastructureUrl}`,
           picked: {
             button_delete_stack: null,
             edit_infrastructure_path: null,
@@ -171,7 +192,7 @@
         moment.locale(queryString.lang);
       },
       methods: {
-        leave(el, done) {
+        leave(el) {
           $(el).fadeOut('normal');
         },
         can_edit() {
@@ -187,29 +208,29 @@
           return (this.picked.id);
         },
         delete_stack() {
-          delete_stack(this.picked.id);
+          deleteStack(this.picked.id);
           this.reload();
         },
-        show_infra(item_id) {
-          this.show_infra_and_rewrite_url(item_id, '');
+        show_infra(itemId) {
+          this.show_infra_and_rewrite_url(itemId, '');
         },
         show_sched() {
           this.show_infra_and_rewrite_url(this.picked.id, 'show_sched');
           this.reload();
         },
-        show_infra_and_rewrite_url(infra_id, infra_oepn_tab) {
-          const prev_infra_id = this.$route.params.infra_id;
-          this.infra_initial_tab = infra_oepn_tab;
+        show_infra_and_rewrite_url(infraId, infraOepnTab) {
+          const prevInfraId = this.$route.params.infra_id;
+          this.infra_initial_tab = infraOepnTab;
           router.push({
             name: 'infra',
             params: {
-              infra_id,
+              infra_id: infraId,
             },
             query: queryString,
           });
           this.infra_initial_tab = '';
-          if (String(prev_infra_id) === String(infra_id)) {
-            this.$refs.infrastructure.reset(infra_oepn_tab);
+          if (String(prevInfraId) === String(infraId)) {
+            this.$refs.infrastructure.reset(infraOepnTab);
           }
         },
         detach_infra() {
@@ -222,8 +243,8 @@
         },
       },
       mounted() {
-        this.$nextTick(function () {
-          show_infra_initialize(this.show_infra_and_rewrite_url);
+        this.$nextTick(function ready() {
+          showInfraInitialize(this.show_infra_and_rewrite_url);
         });
       },
     };
@@ -248,7 +269,7 @@
   }
 
   if ($('#KeypairFormGroup').length) {
-    const keypair_form_group = new Vue({
+    new Vue({
       el: '#KeypairFormGroup',
       data: {
         input_type: 'input',
@@ -259,6 +280,6 @@
 
   $(document).on('click', '.create_ec2_key', (e) => {
     e.preventDefault();
-    new_ec2_key();
+    newEc2Key();
   });
-}());
+}
