9c5e35c9184b5506a0a3998ee56766dc67052158
EC2 Default User <ec2-user@ip-172-31-20-213.ap-northeast-1.compute.internal>
2019-07-04 04:04:17 +0000

    helper.jsのリファクタリング

--- app/assets/javascripts/infrastructures/helper.js
+++ app/assets/javascripts/infrastructures/helper.js
@@ -1,8 +1,8 @@
-const modal = require('modal');
+const modal = require('../modal');
 
 // Vueに登録したfilterを、外から見る方法ってないのかな。
-const jsonParseErr = function (str) {
-  if (_.trim(str) === '') {
+const jsonParseErr = (str) => {
+  if (str.trim() === '') {
     return 'JSON String is empty. Please input JSON.';
   }
   try {
@@ -10,47 +10,42 @@ const jsonParseErr = function (str) {
   } catch (ex) {
     return ex.message;
   }
+  return undefined;
 };
 
-const toLocaleString = function (datetext) {
+const toLocaleString = (datetext) => {
   const date = new Date(datetext);
   return date.toLocaleString();
 };
 
 
 // Utilities
-const alert_success = function (callback) {
-  return function (msg, is_html) {
-    const func = (is_html) ? modal.AlertHTML : modal.Alert;
-    const dfd = func(t('infrastructures.infrastructure'), msg);
-    if (callback) {
-      dfd.done(callback);
-    }
-  };
+const alertSuccess = callback => (msg, isHtml) => {
+  const func = (isHtml) ? modal.AlertHTML : modal.Alert;
+  const dfd = func(t('infrastructures.infrastructure'), msg);
+  if (callback) {
+    dfd.done(callback);
+  }
 };
 
-const alert_danger = function (callback) {
-  return function (msg, is_html) {
-    if (!jsonParseErr(msg) && JSON.parse(msg).error) {
-      modal.AlertForAjaxStdError(callback)(msg);
-    } else {
-      const func = (is_html) ? modal.AlertHTML : modal.Alert;
-      const dfd = func(t('infrastructures.infrastructure'), msg, 'danger');
-      if (callback) { dfd.done(callback); }
-    }
-  };
+const alertDanger = callback => (msg, isHtml) => {
+  if (!jsonParseErr(msg) && JSON.parse(msg).error) {
+    modal.AlertForAjaxStdError(callback)(msg);
+  } else {
+    const func = (isHtml) ? modal.AlertHTML : modal.Alert;
+    const dfd = func(t('infrastructures.infrastructure'), msg, 'danger');
+    if (callback) { dfd.done(callback); }
+  }
 };
 
-const alert_and_show_infra = function (infra_id) {
-  return alert_danger(() => {
-    require('infrastructures/show_infra').show_infra(infra_id);
-  });
-};
+const alertAndShowInfra = infraId => alertDanger(() => {
+  require('../infrastructures/show_infra').show_infra(infraId);
+});
 
 module.exports = {
   jsonParseErr,
   toLocaleString,
-  alert_success,
-  alert_danger,
-  alert_and_show_infra,
+  alert_success: alertSuccess,
+  alert_danger: alertDanger,
+  alert_and_show_infra: alertAndShowInfra,
 };
