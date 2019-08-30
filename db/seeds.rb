7d9afacc725eb43b3c7bf1271ae886ecb9a51a3c
rubysukisuki326 <12824078+rubysukisuki326@users.noreply.github.com>
2019-08-30 06:20:56 +0000

    Fix: rake db:seedで冪等性ミスを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -95,6 +95,6 @@ template_paths.each do |path|
   parsed = JSON.parse(value)
   detail = parsed['Description']
 
-  CfTemplate.where(name: name, format: 'JSON', infrastructure_id: nil).delete_all
+  CfTemplate.where(name: name, format: CfTemplate.formats[:JSON], infrastructure_id: nil).delete_all
   CfTemplate.create(name: name, format: 'JSON', detail: detail, value: value)
 end
