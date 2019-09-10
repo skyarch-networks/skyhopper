8bf92491ecb57904d623362ffbb86da3c8148e9c
rubysukisuki326 <12824078+rubysukisuki326@users.noreply.github.com>
2019-09-10 02:54:52 +0000

    Fix: spec/models/servertest_spec.rbをリファクタリング

--- spec/models/servertest_spec.rb
+++ spec/models/servertest_spec.rb
@@ -8,9 +8,15 @@
 
 require_relative '../spec_helper'
 
-# TODO: refactor
 describe Servertest, type: :model do
   let(:klass) { Servertest }
+  let(:infra_id) { 1 }
+
+  before(:each) do
+    klass.create(infrastructure_id: nil, name: 'recipe_apache2', value: 'code hoge', category: :serverspec)
+    klass.create(infrastructure_id: infra_id, name: 'for_infra1', value: 'code fuga', category: :awspec)
+    klass.create(infrastructure_id: (infra_id + 1), name: 'for_infra2', value: 'code piyo', category: :awspec)
+  end
 
   describe 'with validation' do
     describe 'column value' do
@@ -24,23 +30,17 @@ describe Servertest, type: :model do
     end
   end
 
-  before(:each) do
-    klass.create(infrastructure_id: nil, name: 'recipe_apache2', value: 'code hoge', category: :serverspec)
-    klass.create(infrastructure_id: 1, name: 'for_infra1', value: 'code fuga', category: :awspec)
-    klass.create(infrastructure_id: 2, name: 'for_infra2', value: 'code piyo', category: :awspec)
-  end
-
   it { is_expected.to respond_to(:name) }
   it { is_expected.to respond_to(:infrastructure_id) }
   it { is_expected.to respond_to(:value) }
 
   describe '.for_infra' do
-    it 'return array' do
-      expect(klass.for_infra(1)).to be_kind_of Array
+    it 'should return array' do
+      expect(klass.for_infra(infra_id)).to be_kind_of Array
     end
 
-    it 'return size == 2' do
-      expect(klass.for_infra(1).size).to eq 2
+    it 'should return size is 2' do
+      expect(klass.for_infra(infra_id).size).to eq 2
     end
   end
 
@@ -51,19 +51,18 @@ describe Servertest, type: :model do
   end
 
   describe '.to_file' do
-    it 'return spec path' do
-      expect(klass.to_file(1)).to be_kind_of String
+    it 'should return spec path' do
+      id = klass.first.id
+      expect(klass.to_file(id)).to be_kind_of String
     end
   end
 
   describe '.create_rds' do
-    let(:rds) { double('rds', { engine_type: '', endpoint_address: '' }) }
-    user = 'hoge'
-    pass = 'passwd'
-    infrastructure_id = 1
-
     it 'new Servertest instance' do
-      expect(klass.create_rds(rds, user, pass, infrastructure_id)).to be_kind_of klass
+      rds = double('rds', { engine_type: '', endpoint_address: '' })
+      user = 'hoge'
+      pass = 'passwd'
+      expect(klass.create_rds(rds, user, pass, infra_id)).to be_kind_of klass
     end
   end
 end
