e0cb61413fd4921970fe3ecf604df24ba437afa8
rubysukisuki326 <12824078+rubysukisuki326@users.noreply.github.com>
2019-10-01 07:25:06 +0000

    Fix: production環境の設定を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ SkyHopper::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
