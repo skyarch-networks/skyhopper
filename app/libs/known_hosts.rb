594a73df443c8b691d90d62b1766af2fea5a6787
rubysukisuki326 <12824078+rubysukisuki326@users.noreply.github.com>
2019-11-05 04:22:36 +0000

    Update: app/libs/known_hosts.rb

--- app/libs/known_hosts.rb
+++ app/libs/known_hosts.rb
@@ -12,6 +12,16 @@ module KnownHosts
       true
     end
 
+    def scan_and_update_keys(domain_name)
+      delete_keys(domain_name)
+      scan_and_add_keys(domain_name)
+    end
+
+    def match_remote_key?(domain_name)
+      keys = scan_keys(domain_name)
+      includes_keys_partial?(keys)
+    end
+
     def scan_keys(domain_name)
       command = "ssh-keyscan #{Shellwords.escape(domain_name)}"
       result = exec_command(command)
@@ -40,6 +50,32 @@ module KnownHosts
       Dir.mkdir(ssh_dir, 0o700) unless Dir.exist?(ssh_dir)
 
       File.open(known_hosts_path, mode: 'w', perm: 0o600).close unless File.exist?(known_hosts_path)
+    end
+
+    def includes_keys_partial?(keys)
+      raise ::ArgumentError, '"keys" is allow only Array' unless keys.is_a?(Array)
+
+      pub_keys = keys.map do |known_host_line|
+        pub_key_part(known_host_line)
+      end
+
+      init_ssh_dir
+      File.open(known_hosts_path, mode: 'r') do |file|
+        file.each_line do |line|
+          return true if pub_keys.any? { |pub_key| line.include?(pub_key) }
+        end
+      end
+      false
+    end
+
+    def delete_keys(domain_name)
+      command = "ssh-keygen -R #{Shellwords.escape(domain_name)}"
+      exec_command(command)
+    end
+
+    def pub_key_part(known_host_line)
+      # known_host_line example: domain_name_or_ip1,domain_name_or_ip2 ssh-rsa XXXXXXXXXX
+      known_host_line.split(nil).last
     end
 
     def exec_command(command)
