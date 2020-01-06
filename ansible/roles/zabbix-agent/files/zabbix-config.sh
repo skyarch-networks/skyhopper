3b927c86174971a4962a0b0fb844d25c25e08d7c
hiyuki2578 <tomoka319@gmail.com>
2020-01-06 04:05:54 +0000

    [WIP] ダウングレードできるようにした

--- ansible/roles/zabbix-agent/files/zabbix-config.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cp /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf.orig
-sed -i -e "s/Server=127.0.0.1/Server=$1/i" /etc/zabbix/zabbix_agentd.conf
-sed -i -e "s/ServerActive=127.0.0.1/ServerActive=$1/i" /etc/zabbix/zabbix_agentd.conf
-sed -i -e "s/Hostname=Zabbix server/Hostname=$2/i" /etc/zabbix/zabbix_agentd.conf
