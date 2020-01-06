c42f4f6afe65982eb17b34c4119f8dc6dd166e1f
rubysukisuki326 <12824078+rubysukisuki326@users.noreply.github.com>
2020-01-06 09:25:12 +0000

    Update: Version表記を2.4.0に変更

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,7 @@ module SkyHopper
     config.active_job.queue_adapter = :sidekiq
 
     # Version information
-    config.my_version = 'Version 2.3.0'
+    config.my_version = 'Version 2.4.0'
 
     config.browserify_rails.paths << %r{frontend/}
   end
